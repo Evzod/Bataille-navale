5ec9f17d7d471dfd7e76b383e0dde6a63cdf1e2b
Evzod <evanzoppi@gmail.com>
2025-09-26 19:06:57 +0200

    Test 1
    1er upload

---
+++ src/EssaiFenetre1.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+public class EssaiFenetre1
+{
+public static void main(String[] args)
+{
+Frame f =new Frame("Ma première fenêtre"); //Création d’une fenêtre (un objet de la classe Frame) avec un titre
+Button b= new Button("coucou"); //Création du bouton ayant pour label « coucou »
+f.add(b); //Ajout du bouton dans la fenêtre
+f.pack();
+f.setVisible(true);
+//On demande à la fenêtre de choisir la taille minimum avec pack() et de se rendre visible avec show()
+}
+}
---
+++ src/GameMain.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import javax.awt.*;
+import javax.awt.event.*;
+
+public class GameMain {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
