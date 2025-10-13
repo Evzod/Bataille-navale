91cd83e1ac49e4e4454cd47856c1730111d81be6
Evzod <evanzoppi@gmail.com>
2025-10-13 12:50:35 +0200

    essai fenêtre 2

---
+++ src/EssaiFenetre2.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+public class EssaiFenetre2 implements ActionListener
+{
+public static void main(String[] args)
+{
+Frame f =new Frame("Fenêtre"); //Création d’une fenêtre (un objet de la classe Frame) avec un titre
+Button b= new Button("cliquer"); //Création du bouton ayant pour label « coucou »
+f.add(b/*, BorderLayout.CENTER*/); //Ajout du bouton dans la fenêtre
+b.setSize(50,50);
+b.addMouseListener();
+/*f.pack();*/
+f.setSize(500,500);
+f.setVisible(true);
+//On demande à la fenêtre de choisir la taille minimum avec pack() et de se rendre visible avec show()
+}
+}
