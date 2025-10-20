fc74f3d2a371a0726638dd0c68303bfa81842b5f
Evzod <evanzoppi@gmail.com>
2025-10-20 12:25:42 +0200

    Menu créé
    les boutons marchent

--- src/GameMain.java
+++ src/GameMain.java
@@ -3,10 +3,19 @@ import javax.awt.*;
 import javax.awt.event.*;
 inutile dans ce fichier
 */
+
+import javax.swing.JFrame;
+
 public class GameMain {
 	
 	public static void main (String[] args) {
-		Menu menu1 = new Menu();
+		JFrame f = new JFrame("Bataille navale");
+		f.setSize(1200, 700);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		Menu menu1 = new Menu(f);
+		System.out.println(menu1.getbAppuye());
 		
 	}
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
-public class Menu {
+public class Menu extends JDialog {
 	/*public static void main (String[] args) { // uniquement pour tester le fichier
 		System.out.println("Menu de test lancé");
 		JFrame f = new JFrame("Bataille navale -- Menu");
@@ -10,26 +11,56 @@ public class Menu {
 		f.setVisible(true);
 	}*/
 
-	// constructeur
-	public Menu() {
+	//Variables
+	int bAppuye; //1 = pvp, 2 = pve, 3 = quitter
+
+	// Constructeur
+	public Menu(JFrame f) {
+		super(f, "Bataille navale -- Menu", true );
+		/*Création de la fenêtre
 		JFrame f = new JFrame("Bataille navale -- Menu");
-		f.setSize(900, 600);
+		f.setSize(1200, 700);
 		f.setLocationRelativeTo(null);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel panelBoutons = new JPanel();
-		panelBoutons.setSize(300, 450);
-		panelBoutons.setLayout(BorderLayout);
+		f.setResizable(false);*/
+		//Création des panels imbriqués et des boutons
+		JPanel panelB = new JPanel();
+		JPanel panel = new JPanel();
 		JButton bPvp = new JButton ("Joueur vs Joueur");
 		JButton bPve = new JButton ("Joueur vs Ordinateur");
 		JButton bQuitter = new JButton ("Quitter");
-		panelBoutons.add(bPvp);
-		panelBoutons.add(bPve);
-		panelBoutons.add(bQuitter);
-		f.add(panelBoutons, BorderLayout.CENTER);
+		//Disposition des boutons
+		panelB.setLayout(new GridLayout(3,1, 0, 50));
+		panelB.add(bPvp);
+		panelB.add(bPve);
+		panelB.add(bQuitter);
+		panel.add(panelB);
+		//Ajout des des actions aux boutons
+		bPvp.addActionListener(e -> {
+			bAppuye = 1;
+			f.dispose();
+		});
+		bPve.addActionListener(e -> {
+			bAppuye = 2;
+			f.dispose();
+		});
+		bQuitter.addActionListener(e -> {
+			bAppuye = 3;
+			f.dispose();
+		});
+		//Création du titre et mise en forme
+		JLabel titre = new JLabel("Bataille Navale", JLabel.CENTER);
+		titre.setFont(new Font("Arial", Font.BOLD, 30));
+		titre.setBorder(new EmptyBorder(100, 0, 100, 0));
+		//Ajout des éléments à la fenêtre
+		f.add(titre, BorderLayout.NORTH);
+		f.add(panel, BorderLayout.CENTER);
 		f.setVisible(true);
-		panelBoutons.setVisible(true);
 	}
 
+	public int getbAppuye() {
+		return bAppuye;
+	}
 
 }
 
